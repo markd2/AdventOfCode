479cc218f22a3cd394e6171317dcc1894119a0f4
Mark Dalrymple <markd@borkware.com>
2020-12-01 10:24:59 -0500

    successfully runs reading in from test input file.

--- 2020/day1.f90
+++ 2020/day1.f90
@@ -18,7 +18,7 @@ function findMatch(inArray, inSize, outThing1, outThing2) result(foundMatch)
         if (inArray(i) + inArray(j) == 2020) then
            foundMatch = .true.
            outThing1 = inArray(i)
-           outThing2 = inArraY(j)
+           outThing2 = inArray(j)
            exit
         end if
      end do
@@ -26,20 +26,58 @@ function findMatch(inArray, inSize, outThing1, outThing2) result(foundMatch)
 
 end function
 
+! Doesn't seem to be an easy way to "read this file into an array" given my
+! current caveman familiarity with the language.  So first scan through the file
+! and count the number of elements, and then someone can allocate a properly
+! sized array and then read in to that.
+function fileCount(filename) result(lineCount)
+  use :: iso_fortran_env
+
+  implicit none
+  
+  character(*), intent(in) :: filename
+  integer :: lineCount
+  integer :: readStatus
+
+  lineCount = 0
+
+  open (unit=23, file=filename, status='old', action='read')
+  do
+     read(23, *, iostat=readStatus)
+
+     if (readStatus .eq. iostat_end) then
+        exit
+     end if
+     lineCount = lineCount + 1
+  end do
+  close(23)
+
+end function
+
 
 program preflight
   implicit none
 
+  character(*), parameter :: filename = "day-1-test-input.txt"
   integer :: thing1
   integer :: thing2
   integer :: product
   logical :: found
-  integer, dimension(6) :: data
+  integer, allocatable, dimension(:) :: data
   logical :: findMatch
+  integer :: fileCount
+  integer :: lineCount
+  integer :: i
 
-  data = [1721, 979, 366, 299, 675, 1456]
+  print *, "filecount ", fileCount(filename)
+  lineCount = fileCount(filename)
 
-  found = findMatch(data, 6, thing1, thing2)
+  allocate(data(lineCount))
+  open(unit=23, file=filename, status='old', action='read')
+  read(23, *) (data(i), i=1, lineCount)
+  close(23)
+
+  found = findMatch(data, lineCount, thing1, thing2)
 
   if (found) then
      product = thing1 * thing2
@@ -47,7 +85,6 @@ program preflight
   else
      print *, "not found"
   end if
-  
 
 end program
 
