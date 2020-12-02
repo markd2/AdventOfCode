d1711d3d9c0bc4d12ef47d2b2423b0e674dd84bb
Mark Dalrymple <markd@borkware.com>
2020-12-02 10:58:34 -0500

    ARGH finally got string passing and parsing working.

--- 2020/day2.f90
+++ 2020/day2.f90
@@ -1,25 +1,51 @@
 ! Day 2 - Password Philosophy
 ! https://adventofcode.com/2020/day/2
 
-function isValid(password, letter, upperBound, lowerBound) result(valid)
+module blah
   implicit none
-  character, intent(in) :: password(*)
-  character, intent(in) :: letter
-  integer, intent(in) :: upperBound, lowerBound
-  logical :: valid
+  
+contains
+  
+  function isValid(password, letter, upperBound, lowerBound) result(valid)
+    implicit none
+    character(len=*) :: password
+    character, intent(in) :: letter
+    integer, intent(in) :: upperBound, lowerBound
+    logical :: valid
+    
+    integer :: count
+    integer :: i
+    character :: scan
+    integer :: length
+    
+    count = 0
+    length = len(password)
 
-  valid = .false.
+    print *, "length ", length
 
-end function
+    do i = 1, length
+       scan = password(i:i)
+       print *, "  scan ", scan, " letter ", letter
+       if (scan .eq. letter) then 
+          count = count + 1 
+       end if
+    end do
+    
+    valid = count .ge. upperBound .and. count .le. lowerBound
+    print *, "    valid? ", valid
+
+  end function
+end module
 
 
 program passwordPhilosophy
+  use blah
   implicit none
-  logical :: isValid
+  logical :: isGood
 
-  print *, "1-3 a abcde", isValid("abcde", "a", 1, 3)
-!  print *, "1-3 a abcde", isValid("cdefg", "b", 1, 3)
-!  print *, "1-3 a abcde", isValid("ccccccccc", "a", 2, 9)
+  isGood = isValid("abcde", "a", 1, 3)
+  isGood = isValid("cdefg", "b", 1, 3)
+  isGood = isValid("ccccccccc", "c", 2, 9)
 
 end program
 
