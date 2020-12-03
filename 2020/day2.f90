3611484d9022d9256a4a1d2eb9a8111bfea5474c
Mark Dalrymple <markd@borkware.com>
2020-12-02 20:57:35 -0500

    figured out how to parse the string (finally)

--- 2020/day2.f90
+++ 2020/day2.f90
@@ -6,10 +6,10 @@ module blah
   
 contains
   
-  function isValid(password, letter, upperBound, lowerBound) result(valid)
+  function isValid(password, required, upperBound, lowerBound) result(valid)
     implicit none
     character(len=*) :: password
-    character, intent(in) :: letter
+    character, intent(in) :: required
     integer, intent(in) :: upperBound, lowerBound
     logical :: valid
     
@@ -21,31 +21,70 @@ contains
     count = 0
     length = len(password)
 
-    print *, "length ", length
 
     do i = 1, length
        scan = password(i:i)
-       print *, "  scan ", scan, " letter ", letter
-       if (scan .eq. letter) then 
+       if (scan .eq. required) then 
           count = count + 1 
        end if
     end do
     
     valid = count .ge. upperBound .and. count .le. lowerBound
-    print *, "    valid? ", valid
+    ! print *, "pw ", password, "valid ", valid
 
   end function
 end module
 
 
 program passwordPhilosophy
+  use :: iso_fortran_env
   use blah
   implicit none
   logical :: isGood
+  integer :: goodCount
 
-  isGood = isValid("abcde", "a", 1, 3)
-  isGood = isValid("cdefg", "b", 1, 3)
-  isGood = isValid("ccccccccc", "c", 2, 9)
+  integer :: i
+  integer :: thing1, thing2
+  character :: dummy, dummy2
+  integer :: readStatus
+  character(len=80) :: range, required, password
+
+  character(*), parameter :: filename = "day-2-input.txt"
+
+  open(unit=23, file=filename, status='old', action='read')
+
+  goodCount = 0
+
+  do
+     read(23, *, iostat=readStatus) range, required, password
+     if (readStatus .eq. iostat_end) then
+        exit
+     end if
+
+     ! --- get the range
+     ! replace "-" with a space in the "10-30" range
+     do i = 1, len(range)
+        select case(range(i:i))
+           case('-'); range(i:i) = ' '
+        end select
+     end do
+     ! extract start and end from the string
+     read(range, *) thing1, thing2
+
+     ! --- get the required letter
+     required(2:2) = ''
+
+     isGood = isValid(trim(password), required, thing1, thing2)
+     if (isGood) then
+        goodCount = goodCount + 1
+     end if
+
+  end do
+
+  print *, "good count is ", goodCount
+
+  close(23)
+
 
 end program
 
