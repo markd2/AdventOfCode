c9f259ade076702cb6eda314543f41739bfcce63
Mark Dalrymple <markd@borkware.com>
2020-12-04 20:57:53 -0500

    Solves day 4 part 1. I am not proud of any of this.

--- 2020/day4.f90
+++ 2020/day4.f90
@@ -1,16 +1,18 @@
 ! Day 4 - Passport Processing
 ! https://adventofcode.com/2020/day/4
 
+
 program passportProcessing
   use :: iso_fortran_env
   implicit none
 
-  character(*), parameter :: filename = "day-4-test-input.txt"
+  character(*), parameter :: filename = "day-4-input.txt"
   character(len=256) :: line
   character(50), allocatable :: pairs(:)
   character(50) :: thingie, key, value
   integer :: readStatus
-  integer :: n, i, scan
+  integer :: n, i, scan, seenCount, validCount
+  logical :: done
 
   type :: PassportSeen
      logical :: byrSeen = .false.
@@ -27,22 +29,71 @@ program passportProcessing
 
   open(unit=23, file=filename, status='old', action='read')
 
+  done = .false.
+  validCount = 0
+
   passport = PassportSeen()
-  do
+outer:  do
      ! read a line
      read(23, '(A)', iostat=readStatus) line
      if (readStatus .eq. iostat_end) then
-        exit
+        done = .true.
+        print *, "EOF"
      end if
 
-     ! see how many records on a line
-     n = count(transfer(line, 'a', len(line)) == ":")
+     if (.not. done) then
+        ! see how many records on a line
+        n = count(transfer(line, 'a', len(line)) == ":")
+     end if
 
-     ! no records means done with this passport. Evaluate it
-     if (n .eq. 0) then
-        print *, "NEW RECORD. Last passport: ", passport
+        ! no records means done with this passport. Evaluate it
+     if (done .or. n .eq. 0) then
+        print *, "END OF RECORD. Last passport: ", passport
 
         ! evaluate
+        seenCount = 0
+        if (passport%byrSeen) then
+           seenCount = seenCount + 1
+        end if
+        if (passport%iyrSeen) then
+           seenCount = seenCount + 1
+        end if
+        if (passport%eyrSeen) then
+           seenCount = seenCount + 1
+        end if
+        if (passport%hgtSeen) then
+           seenCount = seenCount + 1
+        end if
+        if (passport%hclSeen) then
+           seenCount = seenCount + 1
+        end if
+        if (passport%eclSeen) then
+           seenCount = seenCount + 1
+        end if
+        if (passport%pidSeen) then
+           seenCount = seenCount + 1
+        end if
+        if (passport%cidSeen) then
+           seenCount = seenCount + 1
+        end if
+
+        if (seenCount .eq. 8) then
+           validCount = validCount + 1
+           print *, "  valid"
+        else if (seenCount .eq. 7) then
+           if (passport%cidSeen) then
+              ! the optional one is there, so this is invalid
+           else
+              validCount = validCount + 1
+              print *, "  valid"
+           end if
+        end if
+        print *, "SEEN ", seenCount
+
+        if (done) then
+           print *, "BAILING OUT"
+           exit outer
+        end if
 
         passport = PassportSeen()
         cycle
@@ -80,7 +131,9 @@ program passportProcessing
 
      deallocate(pairs)
 
-  end do ! each passport
+  end do outer
   close(23)
+
+  print *, "VALID COUNT ", validCount
 
 end program
