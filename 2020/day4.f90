32bb9d966368b3c42d5552a521e97dce079ee062
Mark Dalrymple <markd@borkware.com>
2020-12-05 09:04:24 -0500

    cleaned up using OO features

--- 2020/day4.f90
+++ 2020/day4.f90
@@ -1,8 +1,96 @@
 ! Day 4 - Passport Processing
 ! https://adventofcode.com/2020/day/4
 
+module class_Passport
+  implicit none
+
+  private
+  real :: blah = 1.2345 ! class-wide private constant
+
+  type, public :: Passport
+     logical :: byrSeen = .false.
+     logical :: iyrSeen = .false.
+     logical :: eyrSeen = .false.
+     logical :: hgtSeen = .false.
+     logical :: hclSeen = .false.
+     logical :: eclSeen = .false.
+     logical :: pidSeen = .false.
+     logical :: cidSeen = .false.
+   contains
+     procedure :: see => passport_see
+     procedure :: valid => passport_valid
+  end type
+
+contains
+
+  subroutine passport_see(this, key, value)
+    class(Passport), intent(inout) :: this
+    character(50), intent(in) :: key
+    character(50), intent(in) :: value
+
+    select case(trim(key))
+    case('byr'); this%byrSeen = .true.
+    case('iyr'); this%iyrSeen = .true.
+    case('eyr'); this%eyrSeen = .true.
+    case('hgt'); this%hgtSeen = .true.
+    case('hcl'); this%hclSeen = .true.
+    case('ecl'); this%eclSeen = .true.
+    case('pid'); this%pidSeen = .true.
+    case('cid'); this%cidSeen = .true.
+    end select
+  end subroutine
+
+  function passport_valid(this) result(valid)
+    class(Passport), intent(in) :: this
+    logical :: valid
+
+    integer :: seenCount
+
+    valid = .false.
+    seenCount = 0
+
+    if (this%byrSeen) then
+       seenCount = seenCount + 1
+    end if
+    if (this%iyrSeen) then
+       seenCount = seenCount + 1
+    end if
+    if (this%eyrSeen) then
+       seenCount = seenCount + 1
+    end if
+    if (this%hgtSeen) then
+       seenCount = seenCount + 1
+    end if
+    if (this%hclSeen) then
+       seenCount = seenCount + 1
+    end if
+    if (this%eclSeen) then
+       seenCount = seenCount + 1
+    end if
+    if (this%pidSeen) then
+       seenCount = seenCount + 1
+    end if
+    if (this%cidSeen) then
+       seenCount = seenCount + 1
+    end if
+
+    if (seenCount .eq. 8) then
+       valid = .true.
+       print *, "  valid"
+    else if (seenCount .eq. 7) then
+       if (this%cidSeen) then
+          ! the optional one is there, so this is invalid
+       else
+          valid = .true.
+       end if
+    end if
+
+  end function
+
+end module
 
 program passportProcessing
+  use class_Passport
   use :: iso_fortran_env
   implicit none
 
@@ -12,27 +100,16 @@ program passportProcessing
   character(50) :: thingie, key, value
   integer :: readStatus
   integer :: n, i, scan, seenCount, validCount
-  logical :: done
+  logical :: done, valid
 
-  type :: PassportSeen
-     logical :: byrSeen = .false.
-     logical :: iyrSeen = .false.
-     logical :: eyrSeen = .false.
-     logical :: hgtSeen = .false.
-     logical :: hclSeen = .false.
-     logical :: eclSeen = .false.
-     logical :: pidSeen = .false.
-     logical :: cidSeen = .false.
-  end type
-
-  type(PassportSeen) :: passport
+  type(Passport) :: thePassport
 
   open(unit=23, file=filename, status='old', action='read')
 
   done = .false.
   validCount = 0
 
-  passport = PassportSeen()
+  thePassport = Passport()
 outer:  do
      ! read a line
      read(23, '(A)', iostat=readStatus) line
@@ -46,56 +123,24 @@ outer:  do
         n = count(transfer(line, 'a', len(line)) == ":")
      end if
 
-        ! no records means done with this passport. Evaluate it
+     ! no records means done with this passport. Evaluate it
      if (done .or. n .eq. 0) then
-        print *, "END OF RECORD. Last passport: ", passport
+        print *, "END OF RECORD. Last passport: ", thePassport
 
         ! evaluate
-        seenCount = 0
-        if (passport%byrSeen) then
-           seenCount = seenCount + 1
-        end if
-        if (passport%iyrSeen) then
-           seenCount = seenCount + 1
-        end if
-        if (passport%eyrSeen) then
-           seenCount = seenCount + 1
-        end if
-        if (passport%hgtSeen) then
-           seenCount = seenCount + 1
-        end if
-        if (passport%hclSeen) then
-           seenCount = seenCount + 1
-        end if
-        if (passport%eclSeen) then
-           seenCount = seenCount + 1
-        end if
-        if (passport%pidSeen) then
-           seenCount = seenCount + 1
-        end if
-        if (passport%cidSeen) then
-           seenCount = seenCount + 1
-        end if
 
-        if (seenCount .eq. 8) then
+        ! if valid, incr validCount
+        valid = thePassport%valid()
+        if (valid) then
            validCount = validCount + 1
-           print *, "  valid"
-        else if (seenCount .eq. 7) then
-           if (passport%cidSeen) then
-              ! the optional one is there, so this is invalid
-           else
-              validCount = validCount + 1
-              print *, "  valid"
-           end if
         end if
-        print *, "SEEN ", seenCount
 
         if (done) then
            print *, "BAILING OUT"
            exit outer
         end if
 
-        passport = PassportSeen()
+        thePassport = Passport()
         cycle
      end if
 
@@ -114,18 +159,8 @@ outer:  do
            end select
         end do
 
-        ! split
         read(thingie, *) key, value
-        select case(trim(key))
-        case('byr'); passport%byrSeen = .true.
-        case('iyr'); passport%iyrSeen = .true.
-        case('eyr'); passport%eyrSeen = .true.
-        case('hgt'); passport%hgtSeen = .true.
-        case('hcl'); passport%hclSeen = .true.
-        case('ecl'); passport%eclSeen = .true.
-        case('pid'); passport%pidSeen = .true.
-        case('cid'); passport%cidSeen = .true.
-        end select
+        call thePassport%see(key, value)
         
      end do ! each pair in this line
 
