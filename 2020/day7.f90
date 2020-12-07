cc987a5dc801f58430a47264b20e51341aaa85cd
Mark Dalrymple <markd@borkware.com>
2020-12-07 10:12:29 -0500

    reading raw lines

--- 2020/day7.f90
+++ 2020/day7.f90
@@ -1,21 +1,26 @@
 program handyHaversack
   use hashtbl
+  use :: iso_fortran_env
   implicit none
+
+  character(*), parameter :: filename = "day-7-test-input.txt"
+  character(len=150) :: line   ! longest line in day-7-input.txt is 123 characters
+  integer :: readStatus
+
   type(hash_tbl_sll) :: table
   
   character(len=:), allocatable :: out
+  call table%init(100)
 
-
-  call table%init(23)
-
-  call table%put(key="bright white", val="12")
-
-  call table%get("bright white", out)
-  print *, allocated(out)
-  print *, out
-
-  call table%get("splunge", out)
-  print *, allocated(out)
+  open(unit=23, file=filename, status='old', action='read')
+  do
+     read(23, '(A)', iostat=readStatus) line
+     print *, "read line ", line
+     if (readStatus .eq. iostat_end) then
+        print *, "EOF"
+        exit
+     end if
+  end do
 
 end program
 
