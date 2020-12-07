31d3ca15496c0eaa63f842f308a3c1835a83e995
Mark Dalrymple <markd@borkware.com>
2020-12-07 10:04:06 -0500

    adds example usage of hashtbl

--- 2020/day7.f90
+++ 2020/day7.f90
@@ -1,5 +1,21 @@
 program handyHaversack
+  use hashtbl
+  implicit none
+  type(hash_tbl_sll) :: table
+  
+  character(len=:), allocatable :: out
 
+
+  call table%init(23)
+
+  call table%put(key="bright white", val="12")
+
+  call table%get("bright white", out)
+  print *, allocated(out)
+  print *, out
+
+  call table%get("splunge", out)
+  print *, allocated(out)
 
 end program
 
