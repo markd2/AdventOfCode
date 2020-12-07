4540d22544eaa587910790dc3b3b30599c9a0f33
Mark Dalrymple <markd@borkware.com>
2020-12-07 11:11:32 -0500

    checkpoint f90 attempt.

--- 2020/day7.f90
+++ 2020/day7.f90
@@ -7,6 +7,8 @@ program handyHaversack
   character(len=150) :: line   ! longest line in day-7-input.txt is 123 characters
   integer :: readStatus
 
+  character(len=30), allocatable :: splitBuffer(:)
+
   type(hash_tbl_sll) :: table
   
   character(len=:), allocatable :: out
@@ -15,12 +17,68 @@ program handyHaversack
   open(unit=23, file=filename, status='old', action='read')
   do
      read(23, '(A)', iostat=readStatus) line
-     print *, "read line ", line
+!     print *, "read line ", line
      if (readStatus .eq. iostat_end) then
         print *, "EOF"
         exit
      end if
+     call splitString(line)
+     
   end do
+
+contains
+
+  ! light red bags contain 1 bright white bag, 2 muted yellow bags.
+  subroutine splitString(line)
+    implicit none
+
+    character(len=150), intent(inout) :: line
+    integer :: i
+    character(len=30), allocatable :: split(:)
+    character(len=30), allocatable :: splitLine(:)
+
+    integer :: wordStart, wordEnd, splitIndex
+
+    allocate(split(30))
+
+    ! light red bags contain 1 bright white bag, 2 muted yellow bags.
+    wordStart = 1
+    wordEnd = 1
+    splitIndex = 1
+    do i = 1, len(trim(line))
+       if (line(i:i) .eq. ".") then 
+          wordEnd = wordEnd - 1
+          exit
+       end if
+       if (line(i:i) .eq. " " .or. line(i:i) .eq. ",") then
+          if (wordStart .ne. wordEnd) then
+             allocate(splitLine(wordEnd - wordStart + 1))
+             splitLine = " "
+             split(splitIndex) = line(wordStart:wordEnd)
+             print *, "seeing ", line(wordStart:wordEnd)
+
+!             print *, "found word from ", wordStart, "to ", wordEnd - 1
+!             split(splitIndex) = line(wordStart:wordEnd)
+!             splitIndex = splitIndex + 1
+          end if
+          wordStart = i + 1
+          wordEnd = i + 1
+          cycle
+       end if
+       wordEnd = i + 1
+    end do
+
+    ! mop up last word
+    if (wordStart .ne. wordEnd) then
+       allocate(splitLine(wordEnd - wordStart + 1))
+       splitLine = " "
+       split(splitIndex) = line(wordStart:wordEnd)
+       print *, "seeing ", line(wordStart:wordEnd)
+    end if
+
+!    print *, split
+
+  end subroutine
 
 end program
 
