7cea680f5044bca7d863d3235fe284c0ed499cb7
markd2 <mdalrymple@etsy.com>
2021-12-06 10:02:56 -0500

    Day 6

---
+++ 2021/day-6-1.swift
@@ -0,0 +1,43 @@
+#!/usr/bin/swift sh
+import Foundation
+//import Algorithms // https://github.com/apple/swift-algorithms ~> 1.0.0
+
+let inputFilename = "day-6-prod.txt"
+
+// ----------
+let input = try! String(contentsOfFile: inputFilename)
+let lines = input.split(separator: "\n")
+// ----------
+
+let fishies = lines[0].split(separator: ",").compactMap { Int($0) }
+
+print("so many fishies \(fishies.count)")
+
+var ageCounts: [Int] = Array(repeating: 0, count: 9) // 0 through 8
+
+for fish in fishies {
+    ageCounts[fish] += 1
+}
+
+var day = 0
+let finalDay = 80
+
+while true {
+    // wow.  Swift won't let us popFirst an int array :-(
+    // https://bugs.swift.org/browse/SR-11555
+    // let zeroCount = ageCounts.popFirst() ?? 0
+    let zeroCount = ageCounts[0]
+    ageCounts.removeFirst()
+
+    ageCounts[6] += zeroCount
+    ageCounts.append(zeroCount)
+
+    day += 1
+    if day >= finalDay { break }
+}
+
+let allOfThem = ageCounts.reduce(0, +)
+
+print("so many fishes \(ageCounts) left in the sea, but only \(allOfThem) for Londo and me")
+
+
