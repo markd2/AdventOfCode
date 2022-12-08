a70ba9a959886a32ad850a10a3e0fb9e44704644
Mark Dalrymple <markd@borkware.com>
2022-12-07 20:18:26 -0500

    starts 7.1 code

---
+++ 2022/day-7-1.cpp
@@ -0,0 +1,17 @@
+#import <iostream>
+#import <map>
+
+// No Space Left On Device
+// https://adventofcode.com/2022/day/7
+
+// clang++ -g -std=c++20 -o day-7-1 day-7-1.cpp 
+// ./day-7-1 < day-7-example.txt
+// ./day-7-1 < day-7-prod.txt
+
+int main() {
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        std::cout << line << std::endl;
+    }
+
+}
