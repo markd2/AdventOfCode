fc74762442c284e1d6bb7b3e9d33670729728765
MarkD <markd@borkware.com>
2022-12-09 17:16:17 -0500

    Adds filesystem printer

--- 2022/day-7-1.cpp
+++ 2022/day-7-1.cpp
@@ -1,5 +1,6 @@
 #import <iostream>
 #import <map>
+#import <vector>
 
 // No Space Left On Device
 // https://adventofcode.com/2022/day/7
@@ -8,10 +9,64 @@
 // ./day-7-1 < day-7-example.txt
 // ./day-7-1 < day-7-prod.txt
 
+struct inode {
+    std::map<std::string, inode *> dirent;
+};
+
+inode *root = nullptr;
+inode *cwd = nullptr;
+
+void walk(const inode *root, int indent = 0) {
+    std::vector<std::string> keys;
+    for (std::map<std::string, inode*>::const_iterator it = root->dirent.begin();
+         it !=  root->dirent.end(); ++it) {
+        auto key = it->first;
+        if (key == "..") continue;
+
+        auto spaces = std::string(indent * 2, ' ');
+        std::cout << spaces << key << std::endl;
+
+        auto value = it->second;
+        if (value != nullptr) {
+            walk(value, indent + 1);
+        }
+    }
+}
+
 int main() {
     std::string line;
     while (std::getline(std::cin, line)) {
-        std::cout << line << std::endl;
+        if (line[0] == '$') {
+
+            // --------------------
+            // cd
+            const std::string cd = "$ cd";
+            if (line.compare(0, cd.size(), cd) == 0) {
+                std::cout << line << std::endl;
+                auto dirName = line.substr(5, line.size() - 5);
+
+                if (root == nullptr) {
+                    // we found /
+                    std::cout << "made root";
+                    root = new inode;
+                    cwd = root;
+                    continue;
+                }
+                // check if we've seen this directory yet.  If not,
+                // add it.
+                if (cwd->dirent[dirName] == nullptr) {
+                    inode *newDirectory = new inode;
+                    cwd->dirent[dirName] = newDirectory;
+                    newDirectory->dirent[".."] = cwd;
+                    std::cout << "added new directory |" << dirName << "|" << std::endl;
+                }
+                // then move to it
+                cwd = cwd->dirent[dirName];
+                continue;
+            }
+        }
     }
 
+    walk(root);
+    // std::cout << "blah" << std::endl;
 }
