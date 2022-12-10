6209cf8a0944b189eda0dce51f4b1381b07d4ecd
MarkD <markd@borkware.com>
2022-12-09 19:15:22 -0500

    setting up day 8

--- 2022/day-8-1.cpp
+++ 2022/day-8-1.cpp
@@ -7,12 +7,50 @@
 // ./day-8-1 < day-8-example.txt
 // ./day-8-1 < day-8-prod.txt
 
+using namespace std;
+
+struct Tree {
+    int height;
+    bool visibleFromTop = false;
+    bool visibleFromBottom = false;
+    bool visibleFromLeft = false;
+    bool visibleFromRight = false;
+
+    bool anyVisible();
+};
+
+bool Tree::anyVisible() {
+    return visibleFromTop || visibleFromBottom || visibleFromLeft || visibleFromRight;
+}
+
+std::vector<std::vector<Tree>> forest;
+
+
+void visibleMap() {
+    for (auto stripe: forest) {
+        for (auto tree: stripe) {
+            cout << (tree.anyVisible() ? "#" : ".");
+        }
+        cout << endl;
+    }
+}
 
 int main() {
-    using namespace std;
     string line;
     
     while(getline(cin, line)) {
+        vector<Tree> stripe;
+
+        for (auto ch: line) {
+            Tree tree;
+            tree.height = ch - '0';
+            stripe.push_back(tree);
+        }
+        forest.push_back(stripe);
         cout << line << endl;
     }
+
+    forest[1][2].visibleFromTop = true;
+
+    visibleMap();
 }
