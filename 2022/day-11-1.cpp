8964f906552a8429d4417d202f0ac5ad77cccd1d
MarkD <markd@borkware.com>
2022-12-12 21:46:42 -0500

    Starts processing monkeys

--- 2022/day-11-1.cpp
+++ 2022/day-11-1.cpp
@@ -1,5 +1,6 @@
 #import <iostream>
 #import <vector>
+#import <sstream>
 
 // https://adventofcode.com/2022/day/11
 // Shiny Monkey Dog In The Middle
@@ -10,10 +11,49 @@
 
 using namespace std;
 
+
+struct Monkey {
+    vector<int> items;
+    char op; // + * - /
+    char rhs;  // right hand side.  e.g. "3" or "old"
+    int divisibility;
+    int throwTrue;
+    int throwFalse;
+    
+};
+
+vector<Monkey> monkies;
+
 int main() {
     string line;
 
+    Monkey monkey;
+    monkies.push_back(monkey);
+
     while(getline(cin, line)) {
-        cout << line << endl;
+        // Monkey 0:
+        if (line.rfind("Monkey", 0) == 0) { continue; }
+
+        //   Starting items: 79, 98
+        if (int index = line.rfind("  Starting items: ") == 0) {
+            auto items = line.substr(18, line.size() - 18);
+
+            stringstream ss(items);
+            vector<int> splunge;
+            string token;
+            while (getline(ss, token, ',')) {
+                ss.ignore();
+                splunge.push_back(stoi(token));
+            }
+            monkey.items = splunge;
+            continue;
+        }
+
+        if (line.size() == 0) {
+            monkey = Monkey();
+            monkies.push_back(monkey);
+        }
     }
+
+    cout << monkies.size() << endl;
 }
