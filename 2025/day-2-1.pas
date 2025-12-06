a1af05410891dfe8cd591f3225fcd39ce60184bf
MarkD <markd@borkware.com>
2025-12-06 13:29:12 -0500

    got day-2-1 working with STR$ quasi-bignums.

--- 2025/day-2-1.pas
+++ 2025/day-2-1.pas
@@ -1,65 +1,129 @@
-(* NEEDING TO DO 64-BIT, or use STR$ add-one-to-string because
-some ranges are like 9797966713-9797988709 *)
+
+[INHERIT ('SYS$LIBRARY:STARLET',
+          'SYS$LIBRARY:PASCAL$CLI_ROUTINES',
+          'SYS$LIBRARY:PASCAL$LIB_ROUTINES',
+          'SYS$LIBRARY:PASCAL$SMG_ROUTINES',
+          'SYS$LIBRARY:PASCAL$STR_ROUTINES')]
 
 PROGRAM AOC_DAY_2_1 (INFILE, OUTPUT);
 
 
 TYPE
-    STR255 = STRING(255);
+    STR255 = VARYING [255] OF CHAR;
+    BIGNUM = STR255;
     INTSIZE = INTEGER;
 
 VAR
     INFILE: TEXT;
-    { LINE: STR255; }
+    LINE: STR255;
 
-    RANGE_LOW: INTEGER;
-    RANGE_HIGH: INTEGER;
+    RANGE_LOW: BIGNUM;
+    RANGE_HIGH: BIGNUM;
     COMMA_NOM: CHAR;
 
-    SCAN: INTSIZE;
-    INVALID_SUM: INTSIZE;
+    SCAN: BIGNUM;
+    ONE: BIGNUM;
+    INVALID_SUM: BIGNUM;
 
-FUNCTION IS_VALID_ID(ID: INTSIZE) : BOOLEAN;
+    SPLITPOINT: INTEGER;
+    (* "Bignum" support storage *)
+    SIGN: UNSIGNED;
+    EXP: INTEGER;
+    STATUS: INTEGER;
+    TEMP: BIGNUM;
+
+FUNCTION NUMERIC_COMPARE(THING1, THING2: STR255) : INTEGER; { usual -1/0/+1 }
 VAR
+    LEN1, LEN2: INTEGER;
+BEGIN
+    LEN1 := LENGTH(THING1);
+    LEN2 := LENGTH(THING2);
+
+    IF LEN1 < LEN2 THEN NUMERIC_COMPARE := -1
+    ELSE IF LEN1 > LEN2 THEN NUMERIC_COMPARE := 1
+    ELSE BEGIN
+        IF THING1 < THING2 THEN NUMERIC_COMPARE := -1
+	ELSE IF THING1 > THING2 THEN NUMERIC_COMPARE := 1
+        ELSE NUMERIC_COMPARE := 0;
+    END;
+END;
+
+FUNCTION IS_VALID_ID(VAR ID: BIGNUM) : BOOLEAN;
+VAR
+
     S: VARYING [20] OF CHAR;
     HALF: INTSIZE;
-    LEN: INTSIZE;
-    LEFT: STRING(20);
-    RIGHT: STRING(20);
+    LEFT: STR255;
+    RIGHT: STR255;
+    LEN: INTEGER;
 
 BEGIN
-    IF ID < 10 THEN BEGIN
+    LEN := LENGTH(ID);
+    IF LEN < 2 THEN BEGIN
         IS_VALID_ID := TRUE;
         RETURN;
     END;
 
-    WRITEV(S, ID: 0);
-    LEN := LENGTH(S);
     HALF := TRUNC(LEN / 2);
- WRITELN(ID, " half ", HALF);
-    LEFT := S[1 .. HALF];
-WRITELN("  blah");
-    RIGHT := S[HALF + 1 .. LEN];
-WRITELN("  teleblah");
+    LEFT := ID[ 1 .. HALF ];
+    RIGHT := ID[ HALF + 1 .. LEN];
+
     IS_VALID_ID := LEFT <> RIGHT;
+END;
+
+
+FUNCTION DENORMALIZE(DIGITS: STR255; EXP: INTEGER) : STR255;
+VAR
+    S : STR255;
+    I : INTEGER;
+BEGIN
+    S := DIGITS;
+    FOR I := 1 to EXP DO
+        S := S + '0';
+    DENORMALIZE := S;
 END;
 
 
 (* MAIN *)
 BEGIN
     RESET(INFILE);
-    INVALID_SUM := 0;
+    ONE := "1";
+    INVALID_SUM := "0";
+    SIGN := 0;
+    EXP := 0;
+
     REPEAT
-        READLN(INFILE, RANGE_LOW, COMMA_NOM, RANGE_HIGH);
+        READLN(INFILE, LINE);
+        SPLITPOINT := INDEX(LINE, "-");
+        RANGE_LOW := LINE[1 .. SPLITPOINT - 1];
+        RANGE_HIGH := LINE[ SPLITPOINT + 1 .. LENGTH(LINE)];
+writeln("looking at ", range_low, " -> ", range_high);
 
-{        WRITELN(RANGE_LOW, RANGE_HIGH);}
-
-        FOR SCAN := RANGE_LOW TO RANGE_HIGH DO BEGIN
+        SCAN := RANGE_LOW;
+        REPEAT
             IF NOT IS_VALID_ID(SCAN) THEN BEGIN
-                INVALID_SUM := INVALID_SUM + SCAN;
-{                WRITELN("WOOT ", SCAN, " IS AN INVLID");}
+                WRITELN("    WOOT ", SCAN, " IS INVLID");
+
+                STATUS := STR$ADD(sign, exp, %DESCR INVALID_SUM,
+                                  sign, exp, %DESCR SCAN,
+                                  sign, exp, %DESCR TEMP);
+                IF STATUS <> SS$_NORMAL THEN BEGIN
+                    WRITELN("BAD ADD ", STATUS);
+                END;
+                INVALID_SUM := DENORMALIZE(TEMP, EXP);
+                EXP := 0;
             END;
-        END;
+            STATUS := STR$ADD(sign, exp, %DESCR SCAN,
+                              sign, exp, %DESCR ONE,
+                              sign, exp, %DESCR TEMP);
+            SCAN := DENORMALIZE(TEMP, exp);
+            EXP := 0;
+            IF STATUS <> SS$_NORMAL THEN BEGIN
+                WRITELN("BAD ADD(2) ", STATUS);
+            END;
+
+            
+        UNTIL NUMERIC_COMPARE(SCAN, RANGE_HIGH) = 1;        
 
     UNTIL EOF(INFILE);
     WRITELN(INVALID_SUM);
