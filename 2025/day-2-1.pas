6c99b56ecf8179cf8fe80b8a3d3385c021f3b86d
MarkD <markd@borkware.com>
2025-12-06 13:46:17 -0500

    Clean up some cruft, extract the bignum addition logic

--- 2025/day-2-1.pas
+++ 2025/day-2-1.pas
@@ -1,17 +1,12 @@
 
 [INHERIT ('SYS$LIBRARY:STARLET',
-          'SYS$LIBRARY:PASCAL$CLI_ROUTINES',
-          'SYS$LIBRARY:PASCAL$LIB_ROUTINES',
-          'SYS$LIBRARY:PASCAL$SMG_ROUTINES',
           'SYS$LIBRARY:PASCAL$STR_ROUTINES')]
 
 PROGRAM AOC_DAY_2_1 (INFILE, OUTPUT);
 
-
 TYPE
     STR255 = VARYING [255] OF CHAR;
     BIGNUM = STR255;
-    INTSIZE = INTEGER;
 
 VAR
     INFILE: TEXT;
@@ -19,7 +14,6 @@ VAR
 
     RANGE_LOW: BIGNUM;
     RANGE_HIGH: BIGNUM;
-    COMMA_NOM: CHAR;
 
     SCAN: BIGNUM;
     ONE: BIGNUM;
@@ -31,6 +25,7 @@ VAR
     EXP: INTEGER;
     STATUS: INTEGER;
     TEMP: BIGNUM;
+
 
 FUNCTION NUMERIC_COMPARE(THING1, THING2: STR255) : INTEGER; { usual -1/0/+1 }
 VAR
@@ -50,9 +45,7 @@ END;
 
 FUNCTION IS_VALID_ID(VAR ID: BIGNUM) : BOOLEAN;
 VAR
-
-    S: VARYING [20] OF CHAR;
-    HALF: INTSIZE;
+    HALF: INTEGER;
     LEFT: STR255;
     RIGHT: STR255;
     LEN: INTEGER;
@@ -71,16 +64,33 @@ BEGIN
     IS_VALID_ID := LEFT <> RIGHT;
 END;
 
-
 FUNCTION DENORMALIZE(DIGITS: STR255; EXP: INTEGER) : STR255;
 VAR
-    S : STR255;
     I : INTEGER;
 BEGIN
-    S := DIGITS;
     FOR I := 1 to EXP DO
-        S := S + '0';
-    DENORMALIZE := S;
+        DIGITS := DIGITS + '0';
+    DENORMALIZE := DIGITS;
+END;
+
+
+FUNCTION ADD_BIGNUM(THING1, THING2: BIGNUM) : BIGNUM;
+VAR
+    SIGN: UNSIGNED;
+    EXP: INTEGER;
+    STATUS: INTEGER;
+    TEMP: BIGNUM;
+BEGIN
+    SIGN := 0;
+    EXP := 0;
+
+    STATUS := STR$ADD(sign, exp, %DESCR THING1,
+                      sign, exp, %DESCR THING2,
+                      sign, exp, %DESCR TEMP);
+    IF STATUS <> SS$_NORMAL THEN BEGIN
+        WRITELN("BAD ADD: ", STATUS, THING1, THING2);
+    END;
+    ADD_BIGNUM := DENORMALIZE(TEMP, EXP);
 END;
 
 
@@ -97,33 +107,16 @@ BEGIN
         SPLITPOINT := INDEX(LINE, "-");
         RANGE_LOW := LINE[1 .. SPLITPOINT - 1];
         RANGE_HIGH := LINE[ SPLITPOINT + 1 .. LENGTH(LINE)];
-writeln("looking at ", range_low, " -> ", range_high);
+        WRITELN("looking at ", range_low, " -> ", range_high);
 
         SCAN := RANGE_LOW;
         REPEAT
             IF NOT IS_VALID_ID(SCAN) THEN BEGIN
-                WRITELN("    WOOT ", SCAN, " IS INVLID");
-
-                STATUS := STR$ADD(sign, exp, %DESCR INVALID_SUM,
-                                  sign, exp, %DESCR SCAN,
-                                  sign, exp, %DESCR TEMP);
-                IF STATUS <> SS$_NORMAL THEN BEGIN
-                    WRITELN("BAD ADD ", STATUS);
-                END;
-                INVALID_SUM := DENORMALIZE(TEMP, EXP);
-                EXP := 0;
+                WRITELN("    WOOT ", SCAN, " IS INVALID");
+                INVALID_SUM := ADD_BIGNUM(INVALID_SUM, SCAN);
             END;
-            STATUS := STR$ADD(sign, exp, %DESCR SCAN,
-                              sign, exp, %DESCR ONE,
-                              sign, exp, %DESCR TEMP);
-            SCAN := DENORMALIZE(TEMP, exp);
-            EXP := 0;
-            IF STATUS <> SS$_NORMAL THEN BEGIN
-                WRITELN("BAD ADD(2) ", STATUS);
-            END;
-
-            
-        UNTIL NUMERIC_COMPARE(SCAN, RANGE_HIGH) = 1;        
+            SCAN := ADD_BIGNUM(SCAN, ONE);
+        UNTIL NUMERIC_COMPARE(SCAN, RANGE_HIGH) = 1;
 
     UNTIL EOF(INFILE);
     WRITELN(INVALID_SUM);
